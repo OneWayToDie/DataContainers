787d72a0651858f53c9c2e90048cf954ffb8735d
Danila <Danila@DESKTOP-44LP0PV>
2025-09-05 19:13:14 +0300

    .

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -1,140 +1,146 @@
 ﻿#include<iostream>
 using namespace std;
+//using std::cin;
+//using std::cout;
+//using std::endl;;
 
 #define tab "\t"
 
 
-	class Element
+class Element
+{
+	int Data;
+	Element* pNext;
+	static int count;
+public:
+	Element(int Data, Element* pNext = nullptr)
 	{
-		int Data;
-		Element* pNext;
-		static int count;
-	public:
-		Element(int Data, Element* pNext = nullptr)
-		{
-			this->Data = Data;
-			this->pNext = pNext;
-			count++;
-			cout << "EConstructor:\t" << this << endl;
-		}
-		~Element()
-		{
-			count--;
-			cout << "EDestructor:\t" << this << endl;
-		}
-		friend class ForwardList;
-	};
-	int Element::count = 0;
-
-
-
-	class ForwardList
+		this->Data = Data;
+		this->pNext = pNext;
+		count++;
+		cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
 	{
-		Element* Head;
-	public:
-		ForwardList()
+		count--;
+		cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+};
+int Element::count = 0;
+
+
+
+class ForwardList
+{
+	Element* Head;
+public:
+	ForwardList()
+	{
+		//Конструктор по умолчанию - создаёт пустой список
+		Head = nullptr; //Если список пуст, то его голова указывает на ноль
+		cout << "FLConstructor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		clear();
+		cout << "FLDestructor:\t" << this << endl;
+	}
+	void clear()
+	{
+		while (Head)
 		{
-			//Конструктор по умолчанию - создаёт пустой список
-			Head = nullptr; //Если список пуст, то его голова указывает на ноль
-			cout << "FLConstructor:\t" << this << endl;
+			//https://legacy.cplusplus.com/reference/atomic/atomic/exchange/
+			delete exchange(Head, Head->pNext);
 		}
-		~ForwardList()
+	}
+	//		Adding elements:
+	void push_front(int Data)
+	{
+		//1)Создаём элемент и сохраняем в него добавляемое значение:
+		Element* New = new Element(Data);
+
+		//2)Привязываем новый, созданный элемент к началу списка
+		New->pNext = Head;
+
+		//3) Переносим голову на новый элемент (Отправляем новый элемент в голову):
+		Head = New;
+	}
+
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return push_front(Data);
+
+		Element* New = new Element(Data);
+
+		Element* Temp = Head;
+
+		while (Temp->pNext)Temp = Temp->pNext;
+		
+		Temp->pNext = New;
+	}
+	void Insert(int Index, int Data)
+	{
+		if (Index == 0) return push_front(Data);
+		if (Index >= Element::count)return push_back(Data);
+		//1) Доходим до нужного элемента (элемент перед добавляемым)
+		Element* Temp = Head;
+		for (int i = 0; i < Index-1; i++)Temp = Temp->pNext;
+
+		//2) Создаём добавляемый элемент
+		Element* New = new Element(Data);
+
+		//3) Пристыковываем новый элемент к его следуюущему элементу:
+		New->pNext = Temp->pNext;
+
+		//4) Пристыковываем предыдущий элемент к нововму:
+		Temp->pNext = New;
+	}
+	//						Removing elements:
+	void Erase(int Index)
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+
+		Element* Erased = Temp->pNext;
+		Temp->pNext = Erased->pNext;
+		delete Erased;
+	}
+	void pop_front()
+	{
+		//1) Запоминаем адрес удаляемого элемента:
+		Element* Erased = Head;
+		//2) Исключаем удаляемый элемент из списка:
+		Head = Head->pNext;
+		//3) Удаляем удаляемый элемент из памяти:
+		delete Erased;
+	}
+	void pop_back()
+	{
+		Element* Temp = Head;
+		while (Temp->pNext->pNext != nullptr)Temp = Temp->pNext;
+		delete Temp->pNext;
+		Temp->pNext = nullptr;
+	}
+	void print()const
+	{
+		Element* Temp = Head;	// Temp - это итератор.
+		//Итератор - это указатель, при помощи которого можно перебирать элементы структуры данных.
+		while (Temp)
 		{
-			clear();
-			cout << "FLDestructor:\t" << this << endl;
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+			Temp = Temp->pNext;
 		}
-		void clear()
-		{
-			while (Head)
-			{
-				delete exchange(Head, Head->pNext);
-			}
-		}
-		//		Adding elements:
-		void push_front(int Data)
-		{
-			//1)Создаём элемент и сохраняем в него добавляемое значение:
-			Element* New = new Element(Data);
+		// Temp; - указатель 'Temp'
+		// Temp->...; - Элемент 'Temp'
+		cout << "Количество элементов списка: " << Element::count << endl;
+	}
+};
 
-			//2)Привязываем новый, созданный элемент к началу списка
-			New->pNext = Head;
 
-			//3) Переносим голову на новый элемент (Отправляем новый элемент в голову):
-			Head = New;
-		}
+//#define BASE_CHECK
+#define Check_List
 
-		void push_back(int Data)
-		{
-			if (Head == nullptr)return push_front(Data);
-
-			Element* New = new Element(Data);
-
-			Element* Temp = Head;
-
-			while (Temp->pNext)Temp = Temp->pNext;
-			
-			Temp->pNext = New;
-		}
-		void Insert(int Index, int Data)
-		{
-			if (Index == 0) return push_front(Data);
-			if (Index >= Element::count)return push_back(Data);
-			//1) Доходим до нужного элемента (элемент перед добавляемым)
-			Element* Temp = Head;
-			for (int i = 0; i < Index-1; i++)Temp = Temp->pNext;
-
-			//2) Создаём добавляемый элемент
-			Element* New = new Element(Data);
-
-			//3) Пристыковываем новый элемент к его следуюущему элементу:
-			New->pNext = Temp->pNext;
-
-			//4) Пристыковываем предыдущий элемент к нововму:
-			Temp->pNext = New;
-		}
-		//						Removing elements:
-		void Erase(int Index)
-		{
-			Element* Temp = Head;
-			for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
-
-			Element* Erased = Temp->pNext;
-			Temp->pNext = Erased->pNext;
-			delete Erased;
-		}
-		void pop_front()
-		{
-			//1) Запоминаем адрес удаляемого элемента:
-			Element* Erased = Head;
-			//2) Исключаем удаляемый элемент из списка:
-			Head = Head->pNext;
-			//3) Удаляем удаляемый элемент из памяти:
-			delete Erased;
-		}
-		void pop_back()
-		{
-			Element* Temp = Head;
-			while (Temp->pNext->pNext != nullptr)Temp = Temp->pNext;
-			delete Temp->pNext;
-			Temp->pNext = nullptr;
-		}
-		void print()const
-		{
-			Element* Temp = Head;	// Temp - это итератор.
-			//Итератор - это указатель, при помощи которого можно перебирать элементы структуры данных.
-			while (Temp)
-			{
-				cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
-				Temp = Temp->pNext;
-			}
-			// Temp; - указатель 'Temp'
-			// Temp->...; - Элемент 'Temp'
-			cout << "Количество элементов списка: " << Element::count << endl;
-		}
-	};
-
-#define BASE_CHECK
-//#define Check_List
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -190,5 +196,6 @@ void main()
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	list1.Insert(value, index);
 	list1.print();
+
 #endif
 }
