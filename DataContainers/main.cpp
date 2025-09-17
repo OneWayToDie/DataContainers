abb8f8d4d548436bdc660f8a5a595f1e18825f45
Danila <Danila@DESKTOP-44LP0PV>
2025-09-17 20:33:25 +0300

    Templation_DONE

--- DataContainers/main.cpp
+++ DataContainers/main.cpp
@@ -452,4 +452,13 @@ void main()
 	}
 	cout << endl;
 #endif
+
+	List<double> d_list = { 2.7, 3.14, 5.8, 8.3 };
+	for (double i : d_list)cout << i << tab; cout << endl;
+
+	List<std::string> s_list = { "’орошо", "живЄт", "на", "свете", "¬инни", "ѕух" };
+	for (std::string i : s_list)cout << i << tab; cout << endl;
+	for (List<std::string>::ReverseIterator it = s_list.rbegin(); it != s_list.rend(); ++it)
+		cout << *it << tab;
+	cout << endl;
 }
